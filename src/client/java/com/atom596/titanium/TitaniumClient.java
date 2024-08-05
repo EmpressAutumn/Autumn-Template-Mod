2f97cc89463164cdbcf42d6780605ebbec43005b
Autumn <adamhoffensetz@gmail.com>
2024-08-04 20:20:08 -0400

    Fix Titanium Lantern Chain Transparency

--- src/client/java/com/atom596/titanium/TitaniumClient.java
+++ src/client/java/com/atom596/titanium/TitaniumClient.java
@@ -1,10 +1,14 @@
 package com.atom596.titanium;
 
+import com.atom596.titanium.block.TitaniumBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class TitaniumClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		BlockRenderLayerMap.INSTANCE.putBlock(TitaniumBlocks.TITANIUM_LANTERN,
+				RenderLayer.getTranslucent());
 	}
 }
