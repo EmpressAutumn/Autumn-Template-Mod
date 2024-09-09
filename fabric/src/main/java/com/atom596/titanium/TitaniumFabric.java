6ec8226abe9d24a270f36962b1478e95a99012e5
Autumn <adamhoffensetz@gmail.com>
2024-09-09 19:36:01 -0400

    Fix Fabric Ore Generation pt. 2
    I forgot to commit all the files :(

--- fabric/src/main/java/com/atom596/titanium/TitaniumFabric.java
+++ fabric/src/main/java/com/atom596/titanium/TitaniumFabric.java
@@ -1,7 +1,11 @@
 package com.atom596.titanium;
 
-import com.atom596.titanium.sound.TitaniumSounds;
+import com.atom596.titanium.item.TitaniumItems;
+import com.atom596.titanium.world.TitaniumWorldGeneration;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Items;
 
 public class TitaniumFabric implements ModInitializer {
     
@@ -9,8 +13,52 @@ public class TitaniumFabric implements ModInitializer {
     public void onInitialize() {
         Titanium.initialize();
 
-        //TitaniumWorldGeneration.generateModWorldGen();
+        TitaniumWorldGeneration.generateModWorldGen();
+
+        addCreative();
 
         Titanium.LOGGER.info("Initialized Titanium by EmpressAutumn");
+    }
+
+    private void addCreative() {
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register(content -> {
+            content.addAfter(Items.LIGHT_WEIGHTED_PRESSURE_PLATE, TitaniumItems.TITANIUM_BLOCK.get());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.NATURAL_BLOCKS).register(content -> {
+            content.addAfter(Items.DEEPSLATE_GOLD_ORE, TitaniumItems.TITANIUM_ORE.get());
+            content.addAfter(TitaniumItems.TITANIUM_ORE.get(), TitaniumItems.DEEPSLATE_TITANIUM_ORE.get());
+            content.addAfter(Items.NETHER_QUARTZ_ORE, TitaniumItems.END_TITANIUM_ORE.get());
+            content.addAfter(Items.RAW_GOLD_BLOCK, TitaniumItems.RAW_TITANIUM_BLOCK.get());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> {
+            content.addAfter(Items.LANTERN, TitaniumItems.TITANIUM_LANTERN.get());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(content -> {
+            content.addAfter(Items.GOLDEN_HOE, TitaniumItems.TITANIUM_SHOVEL.get());
+            content.addAfter(TitaniumItems.TITANIUM_SHOVEL.get(), TitaniumItems.TITANIUM_PICKAXE.get());
+            content.addAfter(TitaniumItems.TITANIUM_PICKAXE.get(), TitaniumItems.TITANIUM_AXE.get());
+            content.addAfter(TitaniumItems.TITANIUM_AXE.get(), TitaniumItems.TITANIUM_HOE.get());
+            content.addAfter(Items.FIRE_CHARGE, TitaniumItems.FLIGHT_CHARGE.get());
+            content.addAfter(Items.MUSIC_DISC_PIGSTEP, TitaniumItems.MUSIC_DISC_AMETHYST.get());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT).register(content -> {
+            content.addAfter(Items.GOLDEN_SWORD, TitaniumItems.TITANIUM_SWORD.get());
+            content.addAfter(Items.GOLDEN_AXE, TitaniumItems.TITANIUM_AXE.get());
+            content.addAfter(Items.GOLDEN_BOOTS, TitaniumItems.TITANIUM_HELMET.get());
+            content.addAfter(TitaniumItems.TITANIUM_HELMET.get(), TitaniumItems.TITANIUM_CHESTPLATE.get());
+            content.addAfter(TitaniumItems.TITANIUM_CHESTPLATE.get(), TitaniumItems.TITANIUM_LEGGINGS.get());
+            content.addAfter(TitaniumItems.TITANIUM_LEGGINGS.get(), TitaniumItems.TITANIUM_BOOTS.get());
+            content.addAfter(Items.GOLDEN_HORSE_ARMOR, TitaniumItems.TITANIUM_HORSE_ARMOR.get());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.INGREDIENTS).register(content -> {
+            content.addAfter(Items.RAW_GOLD, TitaniumItems.RAW_TITANIUM.get());
+            content.addAfter(Items.GOLD_NUGGET, TitaniumItems.TITANIUM_NUGGET.get());
+            content.addAfter(Items.GOLD_INGOT, TitaniumItems.TITANIUM_INGOT.get());
+            content.addAfter(Items.FIRE_CHARGE, TitaniumItems.FLIGHT_CHARGE.get());
+            content.addAfter(Items.BLAZE_POWDER, TitaniumItems.END_POWDER.get());
+        });
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.OP_BLOCKS).register(content -> {
+            content.addAfter(Items.DEBUG_STICK, TitaniumItems.TESTING_WAND.get());
+        });
     }
 }
--- fabric/src/main/java/com/atom596/titanium/world/TitaniumWorldGeneration.java
+++ fabric/src/main/java/com/atom596/titanium/world/TitaniumWorldGeneration.java
@@ -1,7 +1,7 @@
-package com.atom596.titanium.world.gen;
+package com.atom596.titanium.world;
 
 public class TitaniumWorldGeneration {
     public static void generateModWorldGen() {
-        //TitaniumOreGeneration.generateOres();
+        TitaniumOreGeneration.generateOres();
     }
 }
